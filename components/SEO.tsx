326fefa0992607b485197d03e6cb1c759725b178
adomaitisc <caadomaitis@gmail.com>
2022-10-17 13:03:27 -0400

    updated favicon and logo

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -46,25 +46,28 @@ export default function SEO() {
       <meta property="og:image:height" content="630" />
 
       {/* Favicon tags */}
-      <link rel="apple-touch-icon" sizes="144x144" href="apple-icon.png" />
+
       <link
-        rel="icon"
-        type="image/png"
-        sizes="144x144"
-        href="android-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="favicon-32x32.png"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="favicon-16x16.png"
+        href="/favicon-16x16.png"
       />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
