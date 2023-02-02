0d85bbc2ebb4fa0aec807d4d72622313a71ef73a
adomaitisc <caadomaitis@gmail.com>
2023-02-02 17:03:09 -0500

    outline not rounding on safari

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import icon from "../public/android-chrome-192x192.png";
 const Navbar = () => {
   return (
     <>
-      <div className="mt-4 mx-auto w-72 fixed top-0 left-0 right-0 flex items-center justify-center bg-black/40 backdrop-blur-lg rounded-full font-kng text-sm outline outline-1 outline-white/20">
+      <div className="mt-4 mx-auto w-72 fixed top-0 left-0 right-0 flex items-center justify-center bg-black/40 backdrop-blur-lg rounded-full font-kng text-sm border border-white/20">
         <div className="md:px-1 py-2 hover:text-gray-300 cursor-pointer select-none touch-none">
           <Link
             style={{ padding: "1rem 0.75rem" }}
